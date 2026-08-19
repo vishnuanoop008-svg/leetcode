3af7e5ae008b725e69c2402434188bd504d686bc
vishnuanoop008-svg <vishnuanoop008@gmail.com>
2026-08-19 11:46:52 +0530

    Time: 58 ms (33.8%), Space: 53.2 MB (92.97%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -9,6 +9,9 @@ var addTwoPromises = async function(promise1, promise2) {
 
     return value1 + value2;
 };
+
+addTwoPromises(Promise.resolve(2),Promise.resolve(2));
+
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
